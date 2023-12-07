55e2d1d1d8ab40efe8539c3e919e5745f1be6ac6
hunlee1 <144081801+hunlee1@users.noreply.github.com>
2023-12-07 14:43:06 +0900

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-public class Solution {
+public class OilDrilling {
     public static void main(String[] args) {
         int[][] land = {
                 {0, 0, 0, 1, 1, 1, 0, 0},
